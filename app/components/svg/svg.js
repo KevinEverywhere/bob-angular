5e73480f09d1fac8ddcc9eb9f2c219a285620958
Kevin Ready <kevin.ready@gmail.com>
2014-11-02 09:00:48 -0800

    First rev with introductory function of SVG

--- app/components/svg/svg.js
+++ app/components/svg/svg.js
@@ -1,149 +1,11 @@
 'use strict';
 
 angular.module('bobApp.svg', ["bobApp", "threeModule", "ngRoute", "ui.router"])
-	.controller('ThreeSVGController', ["$window", "$scope", "$rootScope", "$state", "$stateParams", "LocalCRUDService", "threeCSSService", "threeSphereService", 
-		function ThreeSVGController($window, $scope, $rootScope, $state, $stateParams, LocalCRUDService, threeCSSService, threeSphereService) {
-			/*
-				var d3World=D3Service, countryURL="assets/js/world.json";
-				if(LocalCRUDService.manageLocalCRUD('retrieve', 'd3Data')){
-					d3World.d3Data=LocalCRUDService.manageLocalCRUD('retrieve', 'd3Data');
-					d3World.init(d3World.defaultSVGWidth,d3World.defaultSVGHeight,"#worldDiv", "assets/media/groupedWorld.svg");
-				}else{
-					var _LocalCRUDService=LocalCRUDService;
-					d3World.d3Data=null; 
-					d3.json(countryURL,function(err, d){
-						if(!err){
-							d3World.d3Data=d;
-							_LocalCRUDService.manageLocalCRUD("create",{"key":"d3Data","value":d});
-							d3World.init(d3World.defaultSVGWidth,d3World.defaultSVGHeight,"#worldDiv", "assets/media/groupedWorld.svg");
-						}
-					});
-				}
-
-
-				masterMapApp
-					.controller('SVGController', ["$scope", "$rootScope", "$stateParams", "D3Service", "LocalCRUDService",
-						function SVGController($scope, $rootScope, $stateParams, D3Service, LocalCRUDService) {
-							var d3World=D3Service, countryURL="assets/world.json";
-							if(LocalCRUDService.manageLocalCRUD('retrieve', 'd3Data')){
-								d3World.d3Data=LocalCRUDService.manageLocalCRUD('retrieve', 'd3Data');
-								d3World.init(d3World.defaultSVGWidth,d3World.defaultSVGHeight,"#worldDiv", "assets/groupedWorld.svg");
-							}else{
-								var _LocalCRUDService=LocalCRUDService;
-								d3World.d3Data=null; 
-								d3.json(countryURL,function(err, d){
-									if(!err){
-										d3World.d3Data=d;
-										_LocalCRUDService.manageLocalCRUD("create",{"key":"d3Data","value":d});
-										d3World.init(d3World.defaultSVGWidth,d3World.defaultSVGHeight,"#worldDiv", "assets/groupedWorld.svg");
-									}
-								});
-							}
-						}
-
-
-				var width  = window.innerWidth,
-					height = window.innerHeight;
-
-				// Earth params
-				var radius   = 0.5,
-					segments = 32,
-					rotation = 6;  
-
-				var scene = new THREE.Scene();
-
-				var camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 1000);
-				camera.position.z = 1.5;
-
-				var renderer = new THREE.WebGLRenderer();
-				renderer.setSize(width, height);
-
-				scene.add(new THREE.AmbientLight(0x999999));
-
-				var light = new THREE.DirectionalLight(0xffffff, 1);
-				light.position.set(5,1123,5);
-				scene.add(light);
-				
-			    var sphere = createSphere(radius, segments);
-				sphere.rotation.y = rotation; 
-				//	scene.add(sphere)
-
-					var svgSphere=createSVGSphere(radius, segments);
-					svgSphere.rotation.y = rotation; 
-					scene.add(svgSphere)
-
-				//    var clouds = createClouds(radius, segments);
-				//	clouds.rotation.y = rotation;
-				//	scene.add(clouds)
-
-					var strongLight = new THREE.DirectionalLight(0xffffff, 1);
-					strongLight.position.set(-543,5,5);
-					scene.add(strongLight);
-
-					var stars = createStars(90, 64);
-					scene.add(stars);
-
-					var controls = new THREE.OrbitControls(camera);
-
-					webglEl.appendChild(renderer.domElement);
-
-					render();
-
-					function render() {
-						controls.update();
-					//	sphere.rotation.y += 0.0005;
-					//	clouds.rotation.y += 0.0005;
-						svgSphere.rotation.y += 0.0005;
-						requestAnimationFrame(render);
-						renderer.render(scene, camera);
-					}
-
-					
-
-					function createSVGSphere(radius, segments) {
-						return new THREE.Mesh(
-							new THREE.SphereGeometry(radius, segments, segments),
-							new THREE.MeshPhongMaterial({
-							map:         THREE.ImageUtils.loadTexture('../app/assets/media/groupedWorld.svg')
-							})
-						);
-					}
-
-
-					function createSphere(radius, segments) {
-						return new THREE.Mesh(
-							new THREE.SphereGeometry(radius, segments, segments),
-							new THREE.MeshPhongMaterial({
-							map:         THREE.ImageUtils.loadTexture('../app/assets/media/groupedWorld.svg')
-							})
-						);
-					}
-
-					function createClouds(radius, segments) {
-						return new THREE.Mesh(
-							new THREE.SphereGeometry(radius + 0.003, segments, segments),			
-							new THREE.MeshPhongMaterial({
-								map:         THREE.ImageUtils.loadTexture('images/fair_clouds_4k.png'),
-								transparent: true
-							})
-						);		
-					}
-
-					function createStars(radius, segments) {
-						return new THREE.Mesh(
-							new THREE.SphereGeometry(radius, segments, segments), 
-							new THREE.MeshBasicMaterial({
-								map:  THREE.ImageUtils.loadTexture('images/galaxy_starfield.png'), 
-								side: THREE.BackSide
-							})
-						);
-					}
-
-			*/
-
+	.controller('ThreeSVGController', ["$window", "$scope", "$rootScope", "$state", "$stateParams", "LocalCRUDService", "threeCSSService", 
+		function ThreeSVGController($window, $scope, $rootScope, $state, $stateParams, LocalCRUDService, threeCSSService) {
 			$scope.name='ThreeSVGController';
 			$scope._position={
-				z:100
+				z:-70
 			};
 			$scope._rotation={
 				x:0,
@@ -156,16 +18,20 @@ angular.module('bobApp.svg', ["bobApp", "threeModule", "ngRoute", "ui.router"])
 			$scope.activeParams={};
 			$scope.d3World={};
 			$scope._dir=-1;
-			$scope.incr=.001;
+			$scope.incr=.008;
+			$scope.initialScale=0.4;
 			$scope.currentRotate=180;
 			$scope.maxRotate=190;
 			$scope.minRotate=170;
-			$scope.initWorld=function(){
+			$scope.svgDiv=function(){
+				return document.getElementById($scope._svgDiv)
+			}
+			$scope.initWorld=function(init){
 				var me=this;
-				document.getElementById("svgWrapper").addEventListener("touchend", function(evt){
+				$scope.svgDiv().addEventListener("touchend", function(evt){
 					me.svgCall(evt);
 				}, false);
-				document.getElementById("svgWrapper").addEventListener("click",  function(evt){
+				$scope.svgDiv().addEventListener("click",  function(evt){
 					me.svgCall(evt);
 				}, false);
 			};
@@ -174,9 +40,8 @@ angular.module('bobApp.svg', ["bobApp", "threeModule", "ngRoute", "ui.router"])
 					d3.select("#" + $scope.currentItem).attr('opacity',1)
 				}
 				$scope.currentItem=evt.target.parentNode.id;
-				d3.select("#" + evt.target.parentNode.id).attr('opacity',0.5)
-			
- 			};
+				d3.select("#" + evt.target.parentNode.id).attr('opacity',0.);
+			};
 			$scope.loadSVG=function(xml){
         		try{
 					$scope.importedNode = document.importNode(xml.documentElement, true);
@@ -217,6 +82,12 @@ angular.module('bobApp.svg', ["bobApp", "threeModule", "ngRoute", "ui.router"])
 				$scope.renderer.render($scope.scene, $scope.camera);
 				threeCSSService.render($scope);
 			}
+			var setVendor=function(element, property, value) {
+			  element.style["webkit" + property] = value;
+			  element.style["moz" + property] = value;
+			  element.style["ms" + property] = value;
+			  element.style["o" + property] = value;
+			}
 
 		}
 	])
@@ -233,34 +104,3 @@ angular.module('bobApp.svg', ["bobApp", "threeModule", "ngRoute", "ui.router"])
 		};
 		return threeObj;
 	});
-
-
-/*
-
-
-			$scope.name='ThreeSVGController';
-			$scope.activeAnimations=["animate"];
-			$scope.activeParams=[];
-			$scope.count=0;
-			$scope.maxCount=100;
-			$scope.init=function(elem, _content){
-				console.log("elem=" + elem)
-				if(!this.isInited){
-					threeSphereService.init(elem, $scope, _content);
-					this.isInited=true;
-					render();
-				}
-			}
-			$scope.animate=function(){
-		//		$scope.controls.update();
-				$scope.sphere.rotation.y += 0.0005;
-				$scope.svgSphere.rotation.y += 0.0005;
-			}
-			var render=function() {
-				console.log("THREECSSfooter.render function");
-			//	$window.requestAnimationFrame(render);
-				$scope.renderer.render($scope.scene, $scope.camera);
-				threeCSSService.render($scope);
-			}
-
-*/
