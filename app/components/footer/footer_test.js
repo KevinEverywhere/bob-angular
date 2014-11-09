57e0a3097fa8a0fb780c72ac25f9a325b18daf49
Kevin Ready <kevin.ready@gmail.com>
2014-11-09 12:11:54 -0800

    first heroku build

--- app/components/footer/footer_test.js
+++ app/components/footer/footer_test.js
@@ -7,7 +7,22 @@ describe('bobApp.footer module', function() {
 				var scope = {},
 				ctrl = $controller('ThreeFooterController', {$scope:scope});
 				expect(scope.name).toBe("ThreeFooterController");
+			}));
+
+			it('should have an init function', inject(function($controller) {
+				var scope = {},
+				ctrl = $controller('ThreeFooterController', {$scope:scope});
+				expect(scope.init).toBeDefined();
+			}));
+
+			it('should make room to fit articles', inject(function($controller){
+				var scope={},
+				ctrl=$controller('ThreeFooterController', {$scope:scope});
+				// scope.newsFeedURL="assets/js/test_article.json";
+				scope._width=640;
+				scope.makeScrollFit(5);
+				expect(scope.minMove).toBe(-960);
 			})
-		);
+			)
 	});
 });
