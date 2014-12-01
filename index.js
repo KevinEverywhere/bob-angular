b2dbc5920bdfe9f1883ef1de58504d4b941296e2
Kevin Ready <kevin.ready@gmail.com>
2014-12-01 13:34:20 -0800

    Moved from dist back to app

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var express = require('express')
     , port = process.env.PORT || 3000
     , router = express.Router();
 
-app.use(express.static(__dirname + '/dist/app')); 
+app.use(express.static(__dirname + '/app')); 
 
 app.use(bodyParser.urlencoded({
   extended: true
