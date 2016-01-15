3de587520cd50e9b0c504f73d872f5cc03db5315
Kevin Ready <Kevin.Ready@gmail.com>
2016-01-15 10:59:18 -0800

    managing versions

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,7 +67,7 @@ module.exports=function(grunt){
 		}
 	});
 	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks("grunt-contrib-copy");
+	grunt.loadNpmTasks("grunt-contrib-copy"); 
 	grunt.registerTask("default", ["uglify", "copy"]);
 };
 
